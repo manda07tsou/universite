f5a0e26be032e8e43a3486d5717dc13accf690bd
vonintsoa <rhjmanda@gmail.com>
2024-11-11 11:50:46 +0300

    feat(home): lister etablissement plus visitée

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Adresses;
+use App\Repository\EtablishmentsRepository;
+use App\Repository\FilieresRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(
+        EtablishmentsRepository $er,
+        FilieresRepository $fr
+    ): Response
     {
         return $this->render('home/index.html.twig', [
-            'page' => 'home'
+            'page' => 'home',
+            'etablishments' => $er->findBy([], [], 4),
+            'provinces' => Adresses::$provinces
         ]);
     }
 }
